c65197af2bd747ec4c2b1d94bee369d019e3f9c2
IZO-420 <azlarab.oujdad.izo@gmail.com>
2024-09-10 15:23:14 +0100

    Implement the StoryBook for non React Components

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import VitePluginString  from 'vite-plugin-string';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginString({
+    include: ['**/*.html'],  // Include HTML files
+  }),
+],
 })
